9bee782c0a6923daebaf3091d2515ae6df184b6e
Startale803 <2057094050@qq.com>
2026-07-14 11:15:54 +0800

    Phase 4.5: __init__.py absolute imports + callback exports + retain re-exports

--- app/modules/yolo/__init__.py
+++ app/modules/yolo/__init__.py
@@ -1,5 +1,15 @@
-from ..camera_data import BoundingBoxItem, CameraData, CameraDataStore
-from .batch_detector import BatchDetector
-from .detector import YOLODetector
+from app.modules.camera_data import BoundingBoxItem, CameraData, CameraDataStore
+from app.modules.yolo.batch_detector import BatchDetector
+from app.modules.yolo.detector import YOLODetector
+from app.modules.yolo.callback import get_camera_stats, get_traffic_flow, get_all_traffic_flow
 
-__all__ = ["BatchDetector", "BoundingBoxItem", "CameraData", "CameraDataStore", "YOLODetector"]
+__all__ = [
+    "BatchDetector",
+    "BoundingBoxItem",
+    "CameraData",
+    "CameraDataStore",
+    "YOLODetector",
+    "get_camera_stats",
+    "get_traffic_flow",
+    "get_all_traffic_flow",
+]
